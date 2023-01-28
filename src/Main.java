1ffd0794f452fd1394f8f4cf31907fe944aa2123
Pavel Kryuchkov <mr.p@bk.ru>
2023-01-28 23:33:42 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10000; // стоимость билета
+        int bonus = 20; // стоимость бонусной мили
+        int result = price / bonus; // количество бонусных милей
+
+        System.out.println("Количество бонусных милей - " + result);
+
+}
+}
